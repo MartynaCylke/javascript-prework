73e881c9bf60876982a72e2495c78901d1be7bb3
Martyna <pugdogxxx@gmail.com>
2023-05-10 14:05:02 +0200

    Changes

---
+++ js/script.js
@@ -0,0 +1,49 @@
+function buttonClicked(playerMove) {
+  clearMessages();
+
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
+  const computerMove = getMoveName(randomNumber);
+  printMessage("Zagrałem " + computerMove + ", a Ty " + playerMove);
+
+  displayResult(playerMove.toLowerCase(), computerMove);
+}
+
+function getMoveName(argMoveId) {
+  console.log("wywołano funkcję getMoveName z argumentem: " + argMoveId);
+  if (argMoveId == 1) {
+    return "kamień";
+  } else if (argMoveId == 2) {
+    return "papier";
+  } else if (argMoveId == 3) {
+    return "nożyce";
+  }
+}
+
+function displayResult(argPlayerMove, argComputerMove) {
+  if (argPlayerMove == "papier" && argComputerMove == "kamień") {
+    printMessage("Wygrywasz!");
+  } else if (argPlayerMove == "kamień" && argComputerMove == "nożyce") {
+    printMessage("Wygrywasz!");
+  } else if (argPlayerMove == "nożyce" && argComputerMove == "papier") {
+    printMessage("Wygrywasz!");
+  } else if (argPlayerMove == argComputerMove) {
+    printMessage("Remis!");
+  } else {
+    printMessage("Przegrywasz :(");
+  }
+}
+
+const buttonRock = document.getElementById("button-rock");
+buttonRock.addEventListener("click", function () {
+  buttonClicked("kamień");
+});
+
+const buttonPaper = document.getElementById("button-paper");
+buttonPaper.addEventListener("click", function () {
+  buttonClicked("papier");
+});
+
+const buttonScissors = document.getElementById("button-scissors");
+buttonScissors.addEventListener("click", function () {
+  buttonClicked("nożyce");
+});
